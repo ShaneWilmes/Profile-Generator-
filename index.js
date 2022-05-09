9342ad3ca379e501886b2dc4e6d67f1c3274da9f
ShaneWilmes <shanewilmes@yahoo.com>
2022-05-08 20:45:55 -0600

    added html template

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const inquirer = require('inquirer');
 const manager = require('./lib/manager');
 const engineer = require('./lib/engineer');
 const intern = require('./lib/intern');
+const fs = require('fs');
+
 
 
 
